1695d388ae56f5e0c9c105b89104f27b23b513ce
Malak <malakalsinani@gmail.com>
2026-09-20 01:46:53 +0400

    configure dependency injection for repositories and services in Program.cs

--- backend/SkillTrack.API/Program.cs
+++ backend/SkillTrack.API/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using SkillTrack.API.Data;
 using SkillTrack.API.Repositories;
+using SkillTrack.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,12 +13,44 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<SkillTrackDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<ISkillCategoryRepository, SkillCategoryRepository>();
+builder.Services.AddScoped<ISkillRepository, SkillRepository>();
+builder.Services.AddScoped<ILearningPathRepository, LearningPathRepository>();
+builder.Services.AddScoped<IModuleRepository, ModuleRepository>();
+builder.Services.AddScoped<ILessonRepository, LessonRepository>();
+builder.Services.AddScoped<IResourceRepository, ResourceRepository>();
+builder.Services.AddScoped<IQuizRepository, QuizRepository>();
+builder.Services.AddScoped<IQuestionRepository, QuestionRepository>();
+builder.Services.AddScoped<IQuizAttemptRepository, QuizAttemptRepository>();
+builder.Services.AddScoped<IAssignmentRepository, AssignmentRepository>();
+builder.Services.AddScoped<IAssignmentSubmissionRepository, AssignmentSubmissionRepository>();
+builder.Services.AddScoped<IGoalRepository, GoalRepository>();
+builder.Services.AddScoped<IAchievementRepository, AchievementRepository>();
+builder.Services.AddScoped<IUserAchievementRepository, UserAchievementRepository>();
+builder.Services.AddScoped<IUserSkillRepository, UserSkillRepository>();
+builder.Services.AddScoped<IUserLearningPathRepository, UserLearningPathRepository>();
+builder.Services.AddScoped<ILessonProgressRepository, LessonProgressRepository>();
+
+builder.Services.AddScoped<IPasswordHasherService, PasswordHasherService>();
+builder.Services.AddScoped<IJwtTokenService, JwtTokenService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ISkillCategoryService, SkillCategoryService>();
+builder.Services.AddScoped<ISkillService, SkillService>();
+builder.Services.AddScoped<ILearningPathService, LearningPathService>();
+builder.Services.AddScoped<IModuleService, ModuleService>();
+builder.Services.AddScoped<ILessonService, LessonService>();
+builder.Services.AddScoped<IResourceService, ResourceService>();
+builder.Services.AddScoped<IQuizService, QuizService>();
+builder.Services.AddScoped<IAssignmentService, AssignmentService>();
+builder.Services.AddScoped<IGoalService, GoalService>();
+builder.Services.AddScoped<IAchievementService, AchievementService>();
+builder.Services.AddScoped<IProgressService, ProgressService>();
+builder.Services.AddScoped<IDashboardService, DashboardService>();
 
 
-// Entity-specific repositories and services (IUserRepository, IAuthService, ...)
-// are registered here as each feature is built.
+
 
 builder.Services.AddCors(options =>
 {
@@ -29,7 +62,6 @@ builder.Services.AddCors(options =>
     });
 });
 
-// Authentication/JWT wiring is added in the Auth module step.
 
 var app = builder.Build();
 
